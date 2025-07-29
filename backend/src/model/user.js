aca939e00dba443ee6be3a17f1dfc56e1acaf4e4
Bernard <bernarddira@gmail.com>
2025-07-29 18:23:15 +0200

    update user model

---
+++ backend/src/model/user.js
@@ -0,0 +1,48 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: "string",
+      enum: ["traveler", "admin"],
+      default: "traveler",
+      required: true,
+    },
+    notifyBy: {
+      type: String,
+      enum: ["email", "sms", "dashboard"],
+      default: "email",
+    },
+    riskThreshold: {
+      type: String,
+      enum: ["Low", "Moderate", "High", "Extreme"],
+      default: "Moderate",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const user = mongoose.model("user", userSchema);
