8d350ce65ca5134512afdac15aa25e2e24ba3283
Bernard <bernarddira@gmail.com>
2025-07-29 20:10:55 +0200

    update the alert api endpoints

--- backend/src/app.js
+++ backend/src/app.js
@@ -24,9 +24,11 @@ app.use(express.urlencoded({ extended: true }));
 import userRoutes from "./routes/userRoute.js";
 import monitorDestinationRoutes from "./routes/monitorDestinationRoute.js";
 import healthRoutes from "./routes/healthRoute.js";
+import alertRoutes from "./routes/alertRoute.js";
 
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/destinations", monitorDestinationRoutes);
 app.use("/api/v1/healthcheck", healthRoutes);
+app.use("/api/v1/alert", alertRoutes);
 
 export default app;
